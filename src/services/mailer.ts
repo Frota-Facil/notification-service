0dca9b772ff50b159d48c12fa47a183262e63b08
Sara Maria Alves de Lima <saramalima82@gmail.com>
2026-07-13 22:51:59 -0300

    Corrigindo host do mailhog

--- src/services/mailer.ts
+++ src/services/mailer.ts
@@ -2,7 +2,7 @@ import nodemailer from "nodemailer";
 
 export async function sendEmail(to: string, subject: string, text: string) {
     const transporter = nodemailer.createTransport({
-        host: 'localhost',
+        host: '52.176.18.186',
         port: 1025,
         secure: false
     })
